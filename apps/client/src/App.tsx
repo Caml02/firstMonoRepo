c37e847af89ffd6f42688318037c4cf8d0cbd790
Cristian Morales <cristianmorales3006@gmail.com>
2023-08-21 22:35:53 -0700

    first commit

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <button onClick={async() => {
+    const response = await fetch('/api')
+    const data = await response.text()
+    console.log(data);
+    }}>
+      <span>Click me</span>
+    </button>
+
+  )
+}
+
+export default App;
